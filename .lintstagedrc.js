5aed28406da3dc4a86bcfdec2101a3f09a0a6986
Jonas Tolentino <jonasmarco.ti@gmail.com>
2023-11-02 17:21:08 -0300

    feat: add plop generator

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,6 +3,6 @@ module.exports = {
   '*.{js,jsx,ts,tsx}': (filenames) => [
     `prettier --write ${filenames.join(' ')}`,
     `yarn lint --fix . ${filenames.join(' ')}`,
-    `yarn test -- --findRelatedTests ${filenames.join(' ')}`
+    `yarn test --passWithNoTests ${filenames.join(' ')}`
   ]
 }
